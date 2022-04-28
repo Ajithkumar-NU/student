54168a7dbd5a0d982c7e08f339605ea1fff0a02c
Ajith Komuravelly <ajithkomuravelly111@gmail.com>
2022-04-28 14:55:40 +0530

    final commit

---
+++ form/models.py
@@ -0,0 +1,12 @@
+from pickle import TRUE
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):
+    eid=models.IntegerField(primary_key=TRUE)
+    name=models.CharField(max_length=40)
+    address=models.CharField(max_length=255)
+    city=models.CharField(max_length=20,default="ajmer")
+    gender=models.CharField(max_length=20,default="male")
+    vehicles=models.CharField(max_length=20,default="Car")
+    image = models.ImageField(upload_to='images/',null=True, blank=True)
